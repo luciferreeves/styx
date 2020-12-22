19fa6c89ab00ee43f0a39f203bdb17b0717382a3
Priyansh <bobbyskhs@gmail.com>
2020-12-22 18:05:45 +0530

    Set nodeIntegration to true and add basic HTML

--- main.js
+++ main.js
@@ -10,17 +10,20 @@ function createWindow () {
       minHeight: 600,
       width: 800, 
       height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
   })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'app/markups/home.html'),
+    pathname: path.join(__dirname, 'app/app.html'),
     protocol: 'file:',
     slashes: true
   }))
 
   // Open the DevTools.
-//   win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
